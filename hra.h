3cd226beecb5471e80bb4eaaf512ccf1f4829840
Jakub Rapsik <jrapsik@gmail.com>
2021-12-26 18:10:24 +0100

    Server/Jakub 0.0.3 Nacrt fungovania

---
+++ hra.h
@@ -0,0 +1,11 @@
+//
+// Created by jakub on 26. 12. 2021.
+//
+
+#ifndef SEMESTRALKASERVER_HRA_H
+#define SEMESTRALKASERVER_HRA_H
+
+void add(int a, int b);
+void multiply(int a, int b);
+
+#endif //SEMESTRALKASERVER_HRA_H
