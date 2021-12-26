313ec68071042f48774c078c30df0aab7d19dd6c
filip <filipkotrulik@gmail.com>
2021-12-26 18:20:08 +0100

    filip 0.0.3 header pre hru

---
+++ hra.c
@@ -0,0 +1,8 @@
+//
+// Created by filip on 26. 12. 2021.
+//
+
+#include "hra.h"
+     void tah(){
+
+};
---
+++ hra.h
@@ -0,0 +1,16 @@
+//
+// Created by filip on 26. 12. 2021.
+//
+
+#ifndef SEMESTRALKASERVER_HRA_H
+#define SEMESTRALKASERVER_HRA_H
+
+#include <stdbool.h>
+
+void tah(int stlpec);
+void vypis(char* b, int socket);
+bool kontrolaVyhry();
+
+#endif //SEMESTRALKASERVER_HRA_H
+
+
