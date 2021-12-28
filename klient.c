b5cebd690ba380f52081768b409b0a5cf4f85960
Jakub Rapsik <jrapsik@gmail.com>
2021-12-28 18:26:24 +0100

    Klient/Jakub 0.0.3

--- klient.c
+++ klient.c
@@ -7,73 +7,67 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
 
     int sockfd, n;
     struct sockaddr_in serv_addr;
-    struct hostent* server;
+    struct hostent *server;
 
+    char koniec[] = "koniec";
     char buffer[256];
 
-    if (argc < 3)
-    {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
         return 1;
     }
 
     server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
+    if (server == NULL) {
         fprintf(stderr, "Error, no such host\n");
         return 2;
     }
 
-    bzero((char*)&serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy(
-            (char*)server->h_addr,
-            (char*)&serv_addr.sin_addr.s_addr,
+            (char *) server->h_addr,
+            (char *) &serv_addr.sin_addr.s_addr,
             server->h_length
     );
     serv_addr.sin_port = htons(atoi(argv[2]));
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
+    if (sockfd < 0) {
         perror("Error creating socket");
         return 3;
     }
 
-    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error connecting to socket");
         return 4;
     }
     int hra = 0;
-    while(hra == 0){
+    while (hra == 0) {
         printf("Zadajte cislo stplca kde padne zeton: ");
-        bzero(buffer,256);
+        bzero(buffer, 256);
         fgets(buffer, 255, stdin);
 
         n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0)
-        {
+        if (n < 0) {
             perror("Chyba pri poslati spravy na server");
             return 5;
         }
 
-        bzero(buffer,256);
+        bzero(buffer, 256);
         n = read(sockfd, buffer, 255);
-        if (n < 0)
-        {
+        if (n < 0) {
             perror("Chyba pri prijmani spravi zo servera");
             return 6;
         }
 
-        printf("%s\n",buffer);
-        if (buffer == "koniec"){
+        printf("%s\n", buffer);
+        if (strcmp(koniec, buffer) == 0) {
             hra = 1;
             printf("tu");
         }
